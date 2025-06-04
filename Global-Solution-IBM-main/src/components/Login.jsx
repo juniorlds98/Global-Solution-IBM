ac1b06a63b5449ec64216037d8d924d5d34a4142
Junior Silva <150052476+juniorlds98@users.noreply.github.com>
2025-06-04 00:07:25 -0300

    Add files via upload

---
+++ Global-Solution-IBM-main/src/components/Login.jsx
@@ -0,0 +1,25 @@
+function Login(){
+    return(
+        <section>
+            <div>
+                <img src="" alt="" />
+            </div>
+            <div>
+                <input type="email" />
+                <input type="password" />
+                <div>
+                    <button></button>
+                    <h5>Esqueci a senha</h5>
+                </div>
+                <button>Entrar</button>
+                <h5>Não tem uma conta? Clique aqui</h5>
+                <h5>Faça login por</h5>                
+                <div>
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                    <img src="" alt="" />
+                </div>
+            </div>
+        </section>
+    );
+}
